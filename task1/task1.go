f2be32b29d0060444f6dc6bf40fcc9bbb4bb8ea9
OlehEngineer <73915121+OlehEngineer@users.noreply.github.com>
2022-06-21 20:54:16 +0300

    Update task1.go
    add possibility to monitor Ctrl+C pressing by User.

--- task1/task1.go
+++ task1/task1.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -12,14 +14,37 @@ import (
 func main() {
 	fmt.Println("Hello World")
 	start := time.Now()
-	go time.AfterFunc(10*time.Second, func() {
-		fmt.Println("Goodbye world")
-		os.Exit(0)
-	})
-	Reader := bufio.NewReader(os.Stdin) // read user input
-	Userinput, _ := Reader.ReadString('\n')
-	if Userinput != "" {
-		fmt.Printf("Stopped by user after %0.1f seconds", time.Since(start).Seconds())
-	}
+	// two channels for monitoring user's input
+	sigs := make(chan os.Signal, 1)
+	userStop := make(chan string)
 
+	//count 10 second before closing
+	go func() {
+		time.AfterFunc(10*time.Second, func() {
+			fmt.Println("Goodbye world")
+			os.Exit(0)
+		})
+	}()
+	// monitoring Ctrl+C pressing
+	go func() {
+		signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+		<-sigs
+		// reading of user input with Enter key
+	}()
+	go func() {
+		Reader := bufio.NewReader(os.Stdin) // read user input
+		Userinput, _ := Reader.ReadString('\n')
+		userStop <- Userinput
+	}()
+	//choose first input from user
+	for i := 0; i < 2; i++ {
+		select {
+		case <-sigs:
+			fmt.Printf("Stopped by user after %0.1f seconds", time.Since(start).Seconds())
+			os.Exit(0)
+		case <-userStop:
+			fmt.Printf("Stopped by user after %0.1f seconds", time.Since(start).Seconds())
+			os.Exit(0)
+		}
+	}
 }
