7045b9042507517b47dd57dba4b870a5ddfc9e9f
OlehEngineer <73915121+OlehEngineer@users.noreply.github.com>
2022-06-22 20:20:10 +0300

    Update task2.go
    error handling added.
    Added input size check. At least two values should be entered

--- task2/task2.go
+++ task2/task2.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"sort"
@@ -13,10 +14,22 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Please input numbers for slice you are going to check separated by space")
-	text, _ := reader.ReadString('\n')
-	UserSlice := strings.Fields(text)
+	text, err := reader.ReadString('\n')
+	if err == nil {
+		UserSlice := strings.Fields(text)
+		tooSmallInputCheck := (emptyInputCheck(UserSlice))
 
-	fmt.Println(MinMaxSumOfSlice(inputConverToInt(UserSlice)))
+		if tooSmallInputCheck {
+			fmt.Println(MinMaxSumOfSlice(inputConverToInt(UserSlice)))
+		} else {
+			os.Exit(0)
+		}
+	} else {
+		inputError := errors.New("input is not correct, please try once more")
+		fmt.Println(inputError)
+		os.Exit(0)
+	}
+
 }
 
 // calculate minimum and maximum sum of n-1 slice elements
@@ -41,9 +54,20 @@ func inputConverToInt(UserSlice []string) []int {
 		if err == nil {
 			ConvertedSlice = append(ConvertedSlice, intValue)
 		} else {
-			fmt.Printf("value %v is not an integer. Please try input once more", item)
+			fmt.Printf("value \"%v\" is not an integer. Please try input once more", item)
 			os.Exit(0)
 		}
 	}
 	return ConvertedSlice
+}
+
+//check is the input contain at least two values
+func emptyInputCheck(UserSlice []string) bool {
+	length := len(UserSlice)
+	if length < 2 {
+		fmt.Println("input is less than two values. Input at least two values")
+		return false
+	} else {
+		return true
+	}
 }
