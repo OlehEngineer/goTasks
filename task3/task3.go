9d1baaa4fce3c0ccbc9575d8feddcc345b08e8ef
OlehEngineer <73915121+OlehEngineer@users.noreply.github.com>
2022-06-25 18:29:48 +0300

    Task3 Done
    done task3 with testting file including t.Parallel() method

--- task3/task3.go
+++ task3/task3.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"time"
+)
 
+func main() {
+	incomimgTime := "12:55:25AM"
+
+	fmt.Println(TimeConverter(incomimgTime))
+}
+func TimeConverter(clock string) string {
+	layoutIn := "03:04:05PM"
+	layoutOut := "15:04:05"
+
+	result, err := time.Parse(layoutIn, clock)
+	if err != nil {
+		fmt.Println(err)
+		return ""
+	}
+	return result.Format(layoutOut)
 }
